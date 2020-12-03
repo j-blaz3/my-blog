6158a5ba6072eb18b019f1de0082ae6b8b87f4a9
j-blalock <justin.m.blalock@gmail.com>
2020-12-03 00:56:58 -0600

    added dev dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import HomePage from './pages/HomePage'
-import './App.css';
+import React from "react";
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 function App() {
   return (
